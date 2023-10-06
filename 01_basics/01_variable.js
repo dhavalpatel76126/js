3573ced7d181f6721dc26bc9bb2d284bdd309197
dhavalpatel76126 <dhavalpatel76126@gmail.com>
2023-10-06 12:58:53 +0000

    variable and data type changes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const id= 2;
+let name="dhabal"
+var city="dd"
+
+/*
+dont use var becuase of block scope and functional scope issue
+*/
+
+console.table([id,name,city]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";
+// alert("helo");  we are using not js not a browser
+
+
+//number 2 to power 53
+// big int
+// string => ""
+// boolean => tru/false
+//null => stand alone value
+// undefine => when you not define the value but declared a variable
+// symbol => unique ness 
+
+
+
+// object 
+
+
+console.log(typeof "d");
+
+
+
+// null is object 
+// undeifned type is undefined
